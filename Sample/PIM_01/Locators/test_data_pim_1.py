ff6a077f076ded4a5880ca764af88b5b8ca04c8f
Aruna-RB <aruna.m.balaji@gmail.com>
2024-03-04 15:15:55 +0530

    Initial commit

--- Sample/PIM_01/Locators/test_data_pim_1.py
+++ Sample/PIM_01/Locators/test_data_pim_1.py
@@ -10,12 +10,12 @@ class LoginData:
     username_input_box = "username"
     password_input_box = "password"
     login_xpath = "//button[@type='submit']"
-
-
-class AccessEmployee:
-    pim_tab = "//a[@href='/web/index.php/pim/viewPimModule']"
+    pim_module = '//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a'
+    # pim_tab = '//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a'
     add_employee = "//a[contains(text(),'Add Employee')]"
     save_button = "//button[@type='submit']"
     firstname_input_box = "firstName"
     middlename_input_box = "middleName"
     lastname_input_box = "lastName"
+
+# pytest test_pim_01.py --html=report.html
--- Sample/PIM_01/test_pim_01.py
+++ Sample/PIM_01/test_pim_01.py
@@ -2,24 +2,28 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support import expected_conditions as EC
 import pytest
 from Locators import test_data_pim_1
+import time
 
 
-class CasePim01:
+class TestPim01:
     url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
 
     @pytest.fixture
     def enable_setup(self):
         self.driver = webdriver.Chrome()
-        yield
-        self.driver.close()
-
-    def login(self):
         self.driver.maximize_window()
         self.driver.get(self.url)
         self.driver.implicitly_wait(10)
+        yield
+        # self.driver.close()
+
+    def test_navigate_to_pim(self, enable_setup):
+        # self.driver.maximize_window()
+        # self.driver.get(self.url)
+        # self.driver.implicitly_wait(10)
         self.driver.find_element(By.NAME, test_data_pim_1.LoginData.username_input_box).send_keys(
             test_data_pim_1.EmployeeDetails.username)
         self.driver.find_element(By.NAME, test_data_pim_1.LoginData.password_input_box).send_keys(
@@ -27,21 +31,18 @@ class CasePim01:
         login_button = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, test_data_pim_1.LoginData.login_xpath)))
         login_button.click()
-
-    def navigate_to_pim(self):
-        self.driver.find_element(By.XPATH, test_data_pim_1.AccessEmployee.pim_tab).click()
-        self.driver.find_element(By.XPATH, test_data_pim_1.AccessEmployee.add_employee).click()
-        self.driver.find_element(By.NAME, test_data_pim_1.AccessEmployee.firstname_input_box).send_keys(
+        print("Logged in successfully")
+        pim = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, test_data_pim_1.LoginData.pim_module)))
+        pim.click()
+        # self.driver.find_element(By.XPATH, test_data_pim_1.LoginData.pim_module).click()
+        self.driver.find_element(By.XPATH, test_data_pim_1.LoginData.add_employee).click()
+        self.driver.find_element(By.NAME, test_data_pim_1.LoginData.firstname_input_box).send_keys(
             test_data_pim_1.EmployeeDetails.firstName)
-        self.driver.find_element(By.NAME, test_data_pim_1.AccessEmployee.middlename_input_box).send_keys(
+        self.driver.find_element(By.NAME, test_data_pim_1.LoginData.middlename_input_box).send_keys(
             test_data_pim_1.EmployeeDetails.middleName)
-        self.driver.find_element(By.NAME, test_data_pim_1.AccessEmployee.lastname_input_box).send_keys(
+        self.driver.find_element(By.NAME, test_data_pim_1.LoginData.lastname_input_box).send_keys(
             test_data_pim_1.EmployeeDetails.lastName)
         save = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, test_data_pim_1.AccessEmployee.save_button)))
+            EC.element_to_be_clickable((By.XPATH, test_data_pim_1.LoginData.save_button)))
         save.click()
-
-
-CasePim01().enable_setup()
-CasePim01().login()
-CasePim01().navigate_to_pim()
